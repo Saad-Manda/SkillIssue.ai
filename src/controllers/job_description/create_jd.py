9f9a3b6ce06509230279c1d87a4425a49d5e4e88
Minhaj <mdminhaju29@gmail.com>
2026-03-09 04:19:56 +0530

    Changed fetch of values from pydantic

--- src/controllers/job_description/create_jd.py
+++ src/controllers/job_description/create_jd.py
@@ -12,17 +12,17 @@ from ...models.jd_model import JobDescription as JobDescriptionModel
 async def create_jd(db: AsyncSession, jd_payload: JobDescriptionModel):
     jd = JobDescriptionSchema(
         jd_id = str(uuid4()),
-        job_title = jd_payload.get("job_title"),
-        job_type = jd_payload.get("job_type", "full_time"),
-        loc_type = jd_payload.get("loc_type", "remote"),
-        location = jd_payload.get("location", ""),
-        salary = jd_payload.get("salary", 0.0),
-        min_experience = jd_payload.get("min_experience"),
-        responsibilities = jd_payload.get("responsibilities"),
-        required_qualification = jd_payload.get("required_qualification"),
-        required_skills = jd_payload.get("required_skills"),
-        preferred_skills = jd_payload.get("preferred_skills"),
-        description = jd_payload.get("description")
+        job_title = jd_payload.job_title,
+        job_type = jd_payload.job_type,
+        loc_type = jd_payload.loc_type,
+        location = jd_payload.location,
+        salary = jd_payload.salary or 0.0,
+        min_experience = jd_payload.min_experience,
+        responsibilities = jd_payload.responsibilities,
+        required_qualification = jd_payload.required_qualification,
+        required_skills = jd_payload.required_skills,
+        preferred_skills = jd_payload.preferred_skills,
+        description = jd_payload.description
     )
     db.add(jd)
     await db.commit()
