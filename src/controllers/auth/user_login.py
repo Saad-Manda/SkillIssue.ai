53349cdd28ad9dd8ad20bc634d7e8d080078f999
Saad-Manda <manda_2401ai28@iitp.ac.in>
2026-03-18 13:51:44 +0530

    Added User Login Controller

---
+++ src/controllers/auth/user_login.py
@@ -0,0 +1,19 @@
+from .utils import verify_password, get_user_to_check, create_access_token
+from ...models.user_model import LoginRequest
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+def login(db: AsyncSession, payload: LoginRequest):
+    user = get_user_to_check(db, payload.email, None)
+    if not user:
+        raise HTTPException(status_code=400, detail='Invalid Email')
+    if not verify_password(payload.password, user['hashed_password']):
+        raise HTTPException(status_code=400, detail='Invalid Credentials')
+    
+    data = {
+        'email': user.email,
+        'sub': user.username,
+        'role': ['user']
+    }
+    access_token = create_access_token(data = data)
+    return {'access_token': access_token, 'token_type': 'bearer'}
